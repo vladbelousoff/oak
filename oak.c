59f80451f9636550ca64e1f8d7f39ff4b6781199
Vladislav Belousov <vladbelousov@yahoo.com>
2026-03-11 17:30:34 +0000

    fix(mem): enable memory tracking by calling oak_mem_init()

--- oak.c
+++ oak.c
@@ -6,7 +6,7 @@ int main(const int argc, const char* argv[])
   (void)argc;
   (void)argv;
 
-  // oak_mem_init();
+  oak_mem_init();
 
   oak_lex_t lex;
   oak_lex_tokenize("'Привет, мир!' 289.10\n1000", &lex);
